f4f69361206e9fa5375f2185a8f879f33df2eb3a
Marvin Isaac <misaac@pushnami.com>
2019-08-26 13:11:09 -0400

    :wrench: Setup Bulma with custom component

---
+++ frontend/src/components/Task/Task.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="container">
+        <h1 class="title">Your Tasks</h1>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'Task',
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+</style>

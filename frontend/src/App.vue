d76759fe16d582e4de18938d58001c460f648c86
Marvin Isaac <misaac@pushnami.com>
2019-08-27 15:21:36 -0400

    :wrench: Single-column task list

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,16 +1,34 @@
 <template>
-  <div id="app">
-    <Task/>
-  </div>
+    <div class="section" id="app">
+        <Task v-bind:tasks="tasks"></Task>
+    </div>
 </template>
 
 <script>
 import Task from './components/Task/Task.vue';
 
 export default {
-  name: 'app',
-  components: {
-    Task,
-  },
+    name: 'app',
+    components: {
+        Task,
+    },
+    data() {
+        return {
+            tasks: [
+                {
+                    title: 'Task A',
+                    status: 'done',
+                },
+                {
+                    title: 'Task B',
+                    status: 'todo',
+                },
+                {
+                    title: 'Task C',
+                    status: 'doing',
+                },
+            ],
+        };
+    },
 };
 </script>
--- frontend/src/components/Task/Task.vue
+++ frontend/src/components/Task/Task.vue
@@ -1,15 +1,71 @@
 <template>
-    <div class="container">
-        <h1 class="title">Your Tasks</h1>
+    <div class="columns is-centered">
+        <div class="column is-narrow">
+            <div class="box">
+                <h1 class="title">Your Tasks</h1>
+                <table class="table">
+                    <thead>
+                        <tr>
+                            <th>Task</th>
+                            <th>Status</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr
+                            v-for="(task, index) in tasks"
+                            v-bind:task="task"
+                            v-bind:key="index">
+                            <td>
+                                <input class="input" type="text" disabled
+                                    v-bind:value="task.title">
+                            </td>
+                            <td>
+                                <button class="button" v-if="task.status === 'todo'">
+                                    <font-awesome-icon icon="calendar"></font-awesome-icon>
+                                </button>
+                                <button class="button" v-else-if="task.status === 'doing'">
+                                    <font-awesome-icon pulse icon="spinner"></font-awesome-icon>
+                                </button>
+                                <button class="button is-success" v-else>
+                                    <font-awesome-icon icon="check"></font-awesome-icon>
+                                </button>
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+        </div>
     </div>
 </template>
 
 <script>
 export default {
-  name: 'Task',
+    name: 'Task',
+    props: ['tasks'],
 };
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
+    .table {
+        thead {
+            tr {
+                th {
+                    text-align: center;
+                }
+            }
+        }
+        tbody {
+            tr {
+                td {
+                    input:disabled {
+                        background: white;
+                        border: none;
+                        color: initial;
+                        cursor: default;
+                    }
+                }
+            }
+        }
+    }
 </style>

92d5f13f5ae03c12536cde359b87c3642d1a304c
Marvin Isaac <misaac@pushnami.com>
2019-08-27 13:13:56 -0400

    :wrench: Setup global SCSS file

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -2,9 +2,10 @@ import Vue from 'vue';
 import App from './App.vue';
 import '@/../node_modules/bulma/bulma.sass';
 import './plugins/fontawesome';
+import './styles/base.scss';
 
 Vue.config.productionTip = false;
 
 new Vue({
-  render: h => h(App),
+    render: h => h(App),
 }).$mount('#app');

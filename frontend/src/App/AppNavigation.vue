1dde5d573bb22cd295ce9741c268d503dd894cd5
Marvin Isaac <misaac@pushnami.com>
2019-08-30 18:48:33 -0400

    :wrench: Setup router with custom project structure

---
+++ frontend/src/App/AppNavigation.vue
@@ -0,0 +1,22 @@
+<template>
+    <div id="nav" class="buttons">
+        <router-link to="/" class="button">Home</router-link>
+        <router-link to="/about" class="button">About</router-link>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Navigation'
+}
+</script>
+
+<style lang="scss" scoped>
+@import '../../node_modules/bulma/bulma.sass';
+
+#nav {
+    .router-link-exact-active {
+        @extend .is-success
+    }
+}
+</style>
---
+++ frontend/src/App/Pages/Home/Home.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="@/assets/logo.png"
+        v-on:click="camelcaseTest()">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './HomeHelloWorld.vue'
+
+export default {
+    name: 'home',
+    components: {
+        HelloWorld
+    },
+    methods: {
+        camelcaseTest () {
+            let camelCaseTest = 'Is working!'
+            console.log(camelCaseTest)
+        }
+    }
+}
+</script>

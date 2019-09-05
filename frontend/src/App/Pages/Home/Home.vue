75d45a2093687bfe7678684558a91181ca32d817
Marvin Isaac <misaac@pushnami.com>
2019-09-05 16:51:05 -0400

    :wrench: Setup static URL's for static assets (i.e. images)

--- frontend/src/App/Pages/Home/Home.vue
+++ frontend/src/App/Pages/Home/Home.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="@/assets/logo.png"
+    <img alt="Vue logo" src="/assets/images/logo.png"
         v-on:click="camelcaseTest()">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>

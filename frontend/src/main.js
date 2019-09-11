b7d7bd003665f46e7b06d5f13ee28351faa475e2
Marvin Isaac <misaac@pushnami.com>
2019-09-11 17:43:22 -0400

    :wrench: Add masonry support to project

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,10 +1,12 @@
 import Vue from 'vue'
 import App from './App/App.vue'
 import router from './App/AppRouter'
+import VueMasonry from 'vue-masonry-css'
 import './styles/global.scss'
 import './plugins/fontawesome'
 
 Vue.config.productionTip = false
+Vue.use(VueMasonry)
 
 new Vue({
     router,
